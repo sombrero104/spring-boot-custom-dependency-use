163fc9531c8310e7467e138ce0b5807b1d7ef035
sombrero104 <sombrero.khy@gmail.com>
2019-06-25 11:27:30 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -1,13 +1,34 @@
 package sombrero;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+
+
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+//        SpringApplication application = new SpringApplication(Application.class);
+//        application.setWebApplicationType(WebApplicationType.NONE);
+//        application.run(args);
     }
+
+    /*@Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowlong(60);
+        holoman.setName("sombrero");
+        return holoman;
+    }*/
 
 }
--- src/main/java/sombrero/HolomanRunner.java
+++ src/main/java/sombrero/HolomanRunner.java
@@ -1,4 +1,18 @@
 package sombrero;
 
-public class HolomanRunner {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HolomanRunner implements ApplicationRunner {
+
+    @Autowired
+    Holoman holoman;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(holoman);
+    }
 }
